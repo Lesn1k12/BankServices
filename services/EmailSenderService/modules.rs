1b1388c124c5222eb16d9df9e7b78b048740def6
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-06-30 08:21:44 +0300

    Delete services/EmailSenderService directory

--- services/EmailSenderService/modules.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-pub struct Email {
-    pub to: String,      // Куда идет отправка сообщений.
-    pub subject: String, // Заголовок(та черная хуйня вначале письма)
-    pub body: String,    // Тело запроса. Основаная инфа!
-}
