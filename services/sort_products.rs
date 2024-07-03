6335fae4c9b0d59b5196c6fc10f2f86635a2cc14
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-07-03 17:54:13 +0300

    Update sort_products.rs

--- services/sort_products.rs
+++ services/sort_products.rs
@@ -77,3 +77,19 @@ fn sort_by_address(
     products
 }
 
+use crate::modules::Product;
+use actix_web::web;
+use sqlx::PgPool;
+
+async fn read_all_products(
+    pool: web::Data<PgPool>,
+) -> Result<web::Json<Vec<Product>>, actix_web::Error> {
+    let all_products = sqlx::query_as::<_, Product>("SELECT * FROM products")
+        .fetch_all(pool.get_ref())
+        .await
+        .map_err(|e| {
+            log::error!("Error to read all products -> {}", e);
+            actix_web::error::ErrorInternalServerError("ErrorInternalServerError")
+        })?;
+    Ok(web::Json(all_products))
+}
