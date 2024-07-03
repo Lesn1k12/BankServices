65ce1f388dc8b0f3a500ed9a92a63caf549aeb9e
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-07-03 15:33:31 +0300

    Update sort_products.rs

--- services/sort_products.rs
+++ services/sort_products.rs
@@ -1,37 +1,30 @@
-use crate::modules::{Product, WantedAddress, WantedName, WantedPrice};
+use crate::modules::{Product, WantedSortItem};
 use crate::read_all_products::read_all_products;
 use actix_web::web;
 use sqlx::PgPool;
 
 pub async fn sort_products(
     pool: web::Data<PgPool>,
-    wanted_name: Option<web::Json<WantedName>>,
-    wanted_price: Option<web::Json<WantedPrice>>,
-    wanted_address: Option<web::Json<WantedAddress>>,
+    wanted_sort_item: web::Json<WantedSortItem>,
 ) -> Result<web::Json<Vec<Product>>, actix_web::Error> {
     let all_products = read_all_products(pool).await?.into_inner();
-
-    let sorted_products = match (wanted_name, wanted_price, wanted_address) {
-        (Some(name), _, _) => sort_by_name(all_products, name),
-        (_, Some(price), _) => sort_by_price(all_products, price),
-        (_, _, Some(address)) => sort_by_address(all_products, address),
-        _ => {
-            log::error!("Not correct arguments to sort_product!");
-            all_products
-        }
-    };
-
-    Ok(web::Json(sorted_products))
+    let all_products = sort_by_name(all_products, &wanted_sort_item);
+    let all_products = sort_by_price(all_products, &wanted_sort_item);
+    let all_products = sort_by_address(all_products, &wanted_sort_item);
+    Ok(web::Json(all_products))
 }
 
-fn sort_by_name(mut products: Vec<Product>, wanted_name: web::Json<WantedName>) -> Vec<Product> {
+fn sort_by_name(products: Vec<Product>, wanted_name: &web::Json<WantedSortItem>) -> Vec<Product> {
+    if let Some(wanted_name) = &wanted_name.name {
+        return products
+            .into_iter()
+            .filter(|product| product.name.contains(&wanted_name.to_lowercase()))
+            .collect();
+    }
     products
-        .into_iter()
-        .filter(|product| product.name.contains(&wanted_name.name.to_lowercase()))
-        .collect()
 }
 
-fn sort_by_price(products: Vec<Product>, wanted_price: web::Json<WantedPrice>) -> Vec<Product> {
+fn sort_by_price(products: Vec<Product>, wanted_price: &web::Json<WantedSortItem>) -> Vec<Product> {
     if wanted_price.highest_to_lowest.is_some() {
         sort_by_highest_price(products)
     } else {
@@ -41,7 +34,8 @@ fn sort_by_price(products: Vec<Product>, wanted_price: web::Json<WantedPrice>) -
 
 fn sort_by_lowest_price(mut products: Vec<Product>) -> Vec<Product> {
     products.sort_by(|product, next_product| {
-        product.price
+        product
+            .price
             .partial_cmp(&next_product.price)
             .unwrap_or(std::cmp::Ordering::Equal)
     });
@@ -50,7 +44,8 @@ fn sort_by_lowest_price(mut products: Vec<Product>) -> Vec<Product> {
 
 fn sort_by_highest_price(mut products: Vec<Product>) -> Vec<Product> {
     products.sort_by(|product, next_product| {
-        next_product.price
+        next_product
+            .price
             .partial_cmp(&product.price)
             .unwrap_or(std::cmp::Ordering::Equal)
     });
@@ -58,15 +53,20 @@ fn sort_by_highest_price(mut products: Vec<Product>) -> Vec<Product> {
 }
 
 fn sort_by_address(
-    mut products: Vec<Product>,
-    wanted_address: web::Json<WantedAddress>,
+    products: Vec<Product>,
+    wanted_address: &web::Json<WantedSortItem>,
 ) -> Vec<Product> {
+    if let Some(wanted_country) = &wanted_address.country {
+        if let Some(wanted_region) = &wanted_address.region {
+            return products
+                .into_iter()
+                .filter(|product| {
+                    product.storage_country.to_lowercase() == wanted_country.to_lowercase()
+                        && product.storage_region.to_lowercase() == wanted_region.to_lowercase()
+                })
+                .collect();
+        }
+    };
     products
-        .into_iter()
-        .filter(|product| {
-            product.storage_country.to_lowercase() == wanted_address.country.to_lowercase()
-                && product.storage_region.to_lowercase() == wanted_address.region.to_lowercase()
-        })
-        .collect()
 }
 
