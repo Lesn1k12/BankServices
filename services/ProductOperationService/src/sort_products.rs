eba6f196dbe14822faf5964b5937c5b38421d5d2
Aqueira <aqurawe@gmail.com>
2024-07-06 16:31:10 +0300

    modified sort_products

--- services/ProductOperationService/src/sort_products.rs
+++ services/ProductOperationService/src/sort_products.rs
@@ -9,13 +9,22 @@ pub async fn sort_products(
     let all_products = read_all_products(pool).await?.into_inner();
 
     if let Some(wanted_sort_item) = wanted_sort_item {
-        return Ok(web::Json(merge_sort(all_products, wanted_sort_item)));
+        return Ok(web::Json(merge_sorts(all_products, wanted_sort_item)?));
     };
 
     Ok(web::Json(all_products))
 }
 
-fn merge_sort(mut products: Vec<Product>, wanted_item: web::Json<WantedSortItem>) -> Vec<Product> {
+fn merge_sorts(mut products: Vec<Product>, wanted_item: web::Json<WantedSortItem>) -> Result<Vec<Product>, actix_web::Error> {
+    products = sort_by_name(products, &wanted_item);
+    sort_by_price(&mut products, &wanted_item)?;
+    products = sort_by_terrain(products, &wanted_item);
+    products = sort_by_category(products, &wanted_item);
+
+    Ok(products)
+}
+
+fn sort_by_name(mut products: Vec<Product>, wanted_item: &web::Json<WantedSortItem>) -> Vec<Product>{
     if let Some(wanted_name) = &wanted_item.name {
         products = products
             .into_iter()
@@ -27,54 +36,73 @@ fn merge_sort(mut products: Vec<Product>, wanted_item: web::Json<WantedSortItem>
             })
             .collect();
 
-        products.sort_by(|a, b| a.name.cmp(&b.name));
+        products.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
     };
 
-    if let Some(wanted_price) = wanted_item.lowest_to_highest {
-        if wanted_price {
-            products.sort_by(|product, next_product| {
-                product
-                    .price
-                    .partial_cmp(&next_product.price)
-                    .unwrap_or(std::cmp::Ordering::Equal)
-            });
+    products
+}
+
+fn sort_by_price(products: &mut Vec<Product>, wanted_item: &web::Json<WantedSortItem>) -> Result<(), actix_web::Error>{
+    match (wanted_item.lowest_to_highest, wanted_item.highest_to_lowest){
+        (Some(_), Some(_)) => {
+            log::error!("You can sort by price only with 1 parameter!");
+            return Err(actix_web::error::ErrorInternalServerError("You can sort by price only with 1 parameter!"));
         }
-    };
 
-    if let Some(wanted_price) = wanted_item.highest_to_lowest {
-        if wanted_price {
-            products.sort_by(|product, next_product| {
-                next_product
-                    .price
-                    .partial_cmp(&product.price)
-                    .unwrap_or(std::cmp::Ordering::Equal)
-            });
-        }
-    };
-
-    if let Some(wanted_country) = &wanted_item.country {
-        if let Some(wanted_region) = &wanted_item.region {
-            products = products
-                .into_iter()
-                .filter(|product| {
+        (Some(wanted_price), _) => {
+            if wanted_price {
+                products.sort_by(|product, next_product| {
                     product
-                        .storage_country
-                        .eq_ignore_ascii_case(&wanted_country)
-                        && product.storage_region.eq_ignore_ascii_case(wanted_region)
-                })
-                .collect();
+                        .price
+                        .partial_cmp(&next_product.price)
+                        .unwrap_or(std::cmp::Ordering::Equal)
+                });
+            }
         }
+
+        (_, Some(wanted_price)) => {
+            if wanted_price {
+                products.sort_by(|product, next_product| {
+                    next_product
+                        .price
+                        .partial_cmp(&product.price)
+                        .unwrap_or(std::cmp::Ordering::Equal)
+                });
+            }
+        }
+        (_, _) => {}
+    }
+
+    Ok(())
+}
+
+fn sort_by_terrain(mut products: Vec<Product>, wanted_item: &web::Json<WantedSortItem>) -> Vec<Product>{
+    if let (Some(wanted_country), Some(wanted_region)) = (&wanted_item.country, &wanted_item.region) {
+        products = products
+            .into_iter()
+            .filter(|product| {
+                product
+                    .storage_country
+                    .eq_ignore_ascii_case(&wanted_country)
+                    && product.storage_region.eq_ignore_ascii_case(wanted_region)
+            })
+            .collect();
     };
 
-    if let Some(wanted_category) = &wanted_item.category{
-         products = products
-             .into_iter()
-             .filter(|product| product.category.eq_ignore_ascii_case(&wanted_category))
-             .collect();
+    products
+}
+
+fn sort_by_category(mut products: Vec<Product>, wanted_item: &web::Json<WantedSortItem>) -> Vec<Product>{
+    if let Some(wanted_category) = &wanted_item.category {
+        products = products
+            .into_iter()
+            .filter(|product| product.category.eq_ignore_ascii_case(&wanted_category))
+            .collect();
     }
 
     products
 }
+
 
 async fn read_all_products(
     pool: web::Data<PgPool>,
